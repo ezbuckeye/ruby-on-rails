ad7d321ab6bf287a9342c29aecd2dd0fb45543bc
Edison Zhang <zhang.12789@osu.edu>
2023-05-26 12:27:56 -0500

    setup new rails-api and its tests

---
+++ books-api/config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      resources :categories, only: %i[index create destroy]
+      resources :books, only: %i[index create show update destroy]
+    end
+  end
+end
